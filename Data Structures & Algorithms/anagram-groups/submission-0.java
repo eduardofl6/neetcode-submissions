c5ca61aacfa8e5b903e009a44f926c198c00ba9d
Eduardo Fortes Luiz <eduardofortesluiz6@gmail.com>
2026-05-21 11:19:27 -0300

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> list = new ArrayList<>();
+        HashMap<String, ArrayList<String>> hash = new HashMap();
+        String word;
+        char[] wray;
+
+        for(String s : strs){
+            wray = s.toCharArray();
+            Arrays.sort(wray);
+            word = new String(wray);
+
+            if(!(hash.containsKey(word))){
+                hash.put(word, new ArrayList<String>());
+            }
+            
+            hash.get(word).add(s);
+        }
+
+        Set<String> keys = hash.keySet();
+
+        for(String s : keys){
+            list.add(hash.get(s));
+        }
+
+
+        return list;
+    }
+
+}
