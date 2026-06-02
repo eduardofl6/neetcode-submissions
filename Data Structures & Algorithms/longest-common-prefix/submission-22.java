6f4d152f54a426edf1352b33d4adfd9430b2c15e
Eduardo Fortes Luiz <eduardofortesluiz6@gmail.com>
2026-06-02 19:18:43 -0300

    Add: longest-common-prefix - submission-22

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-22.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        String prefix = "";
+        String small = strs[0];
+
+        for(String s : strs){
+            if(s.equals("")){
+                return prefix;
+            }
+
+            if(s.length() <  small.length()){
+                small = s;
+            }
+        }
+        
+        for(int i = 0; i < small.length(); i++){
+            boolean fact = true;
+            for(int j = 0; j < strs.length; j++){
+                if(strs[j].charAt(i) != small.charAt(i)){
+                    fact = false;
+                    break;
+                }
+            }
+            if(i == 0 && !fact) return prefix;
+            if(fact) prefix += small.charAt(i);
+        }
+
+        return prefix;
+    }
+}
