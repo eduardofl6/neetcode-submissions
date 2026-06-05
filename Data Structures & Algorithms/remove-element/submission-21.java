0f8c4c0db4eb1e3a18cf86e2240fd0af0946367a
Eduardo Fortes Luiz <eduardofortesluiz6@gmail.com>
2026-06-05 16:57:03 -0300

    Add: remove-element - submission-21

---
+++ Data Structures & Algorithms/remove-element/submission-21.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int count = 0;
+        int[] subar = new int [nums.length];     
+
+        for(int i = 0; i < subar.length; i++){
+            subar[i] = 101;
+        }
+
+        for(int i = 0, v = 0; i < nums.length; i++){
+            if(nums[i] != val){
+                subar[v++] = nums[i];
+                count++;
+            }
+        }
+        
+        for(int i = 0; i < subar.length; i++){
+            nums[i] = (subar[i] == 101 ? val : subar[i]);
+            System.out.println(nums[i]);
+        }   
+
+
+    return count;
+    }
+}
